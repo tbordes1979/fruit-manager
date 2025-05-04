c1e9c241da7d6424b5b7ead36f73204a0803e164
Guillaume <mlearnia@gmail.com>
2025-05-04 10:10:51 +0100

    creation du premier dashboard

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from fruit_manager import *
+
+st.title("🍇 Dashboard de la Plantation")
+
+inventaire = ouvrir_inventaire()
+prix = ouvrir_prix()
+tresorerie = ouvrir_tresorerie()
+
+st.header("💰 Trésorerie")
+st.metric(label="Montant disponible", value=f"{tresorerie:.2f} $")
+
+st.header("📦 Inventaire")
+st.table(inventaire)
